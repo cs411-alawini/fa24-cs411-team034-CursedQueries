375b596bc22f0e270fb10d225eb8e87e0fe78b4d
Sneha Sundar <snehas9@illinois.edu>
2024-12-09 22:37:10 -0600

    App.js update with userProvider wrapper

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,32 +1,26 @@
 import './App.css'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserProvider } from './context/UserContext'; // Import UserProvider
 
 import Homepage from "./pages/Homepage";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import MyGroups from './pages/MyGroups';
-import { UserProvider } from './context/UserContext';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <UserProvider>
-      <App />
-  </UserProvider>,
-  document.getElementById('root')
-);
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/mygroups" element = {<MyGroups/>}/>
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/mygroups" element={<MyGroups />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 };
 
